1ada579a2300d11f9d2f28716ef5be156932d0ea
Tony Kim <kimtony0818@gmail.com>
2018-02-25 16:24:54 -0500

    Code used to fetch tweets with '#Olympics'

---
+++ fetchTweet.py
@@ -0,0 +1,73 @@
+import tweepy
+import sys
+import jsonpickle
+import os
+
+
+API_KEY = "1qc5USiKguAFMnqGQKgltENA0"
+API_SECRET = "TmKTatbMbskLmsZd7hoYUAiWzSbNZzxqyTWHsxjSoq5nca8BlP"
+
+# Creating the authentication object
+auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
+# Creating the API object while passing in auth information
+api = tweepy.API(auth, wait_on_rate_limit=True,
+				   wait_on_rate_limit_notify=True)
+
+if (not api):
+    print ("Can't Authenticate")
+    sys.exit(-1)
+
+
+
+
+
+# Continue with rest of code
+searchQuery = '#Olympics'  # this is what we're searching for
+maxTweets = 10 000 # Some arbitrary large number
+tweetsPerQry = 100  # this is the max the API permits
+fName = 'tweets_during_the_olympics.txt' # We'll store the tweets in a text file.
+
+
+# If results from a specific ID onwards are reqd, set since_id to that ID.
+# else default to no lower limit, go as far back as API allows
+sinceId = None
+
+# If results only below a specific ID are, set max_id to that ID.
+# else default to no upper limit, start from the most recent tweet matching the search query.
+max_id = -1
+
+# Updating Progress of Fetching Twitter Data
+tweetCount = 0
+print("Downloading max {0} tweets".format(maxTweets))
+with open(fName, 'w') as f:
+    while tweetCount < maxTweets:
+        try:
+            if (max_id <= 0):
+                if (not sinceId):
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry)
+                else:
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                            since_id=sinceId)
+            else:
+                if (not sinceId):
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                            max_id=str(max_id - 1))
+                else:
+                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                            max_id=str(max_id - 1),
+                                            since_id=sinceId)
+            if not new_tweets:
+                print("No more tweets found")
+                break
+            for tweet in new_tweets:
+                f.write(jsonpickle.encode(tweet._json, unpicklable=False) +
+                        '\n')
+            tweetCount += len(new_tweets)
+            print("Downloaded {0} tweets".format(tweetCount))
+            max_id = new_tweets[-1].id
+        except tweepy.TweepError as e:
+            # Just exit if any error
+            print("some error : " + str(e))
+            break
+
+print ("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
